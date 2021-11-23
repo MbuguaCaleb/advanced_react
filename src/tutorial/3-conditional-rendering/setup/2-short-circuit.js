2a757eb25de46aaf6ae692de75aaf128e4b12e0c
Mbugua Caleb <mbuguacaleb30@gmail.com>
2021-11-23 11:16:38 +0300

    short-circuit-evaaluation

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState()
 
-  return <h2>short circuit</h2>;
-};
+  //if text is true i return text
+  //When false i return hello world
+  const firstValue = text || 'hello world'
 
-export default ShortCircuit;
+  //if text is true i return Hello WOLD
+  //IF FALSE I RETURN TEXT
+  const secondValue = text && 'hello world'
+
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+      <h1>value:{secondValue}</h1> */}
+      {/* I cannot return an expression in my JSX */}
+      {/* {if(2>1){
+    console.log("one")
+    }} */}
+
+      {/* i cannever use an if statement.It will confuse my JSX */}
+
+      <h1>{text || 'Mbugua Caleb'}</h1>
+      {text && <h1>Hello World!</h1>}
+      {!text && <h1>Hello World!</h1>}
+    </>
+  )
+}
+
+export default ShortCircuit
